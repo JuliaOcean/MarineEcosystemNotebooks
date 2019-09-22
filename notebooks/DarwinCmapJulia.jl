c638f160c5458c1b3ec25e8edc5071d578233a7f
gaelforget <gforget@mit.edu>
2019-09-22 16:18:51 -0400

    update commands to use pycmap (opedia is deprecated)

--- notebooks/DarwinCmapJulia.jl
+++ notebooks/DarwinCmapJulia.jl
@@ -13,16 +13,18 @@
 #     name: julia-1.1
 # ---
 
-# This julia notebook uses [Simons' CMAP](https://cmap.readthedocs.io/en/latest/) to depict output of the Darwin model in context of the [CBIOMES](https://cbiomes.org) project.
+# This julia notebook uses [Simons' CMAP](https://cmap.readthedocs.io/en/latest/) to extract Darwin model output from the [CBIOMES](https://cbiomes.org) project.
 #
-# <img src="../figs/cbiomes-01.png" alt="Drawing" style="height: 50px;"/>
+# <img src="../figs/cbiomes-01.png" alt="Drawing" style="height: 100px;"/>
 #
 # **Pre-requisites:**
 #
-# - 1) install the [PyCmap python package and its dependencies](https://github.com/simonscmap/pycmap) using `pip`.
+# - 1) install the [PyCmap](https://github.com/simonscmap/pycmap) python package and its dependencies using `pip`.
 # - 2) compile [PyCall.jl](https://github.com/simonscmap/pycmap) using external python distribution that installed `PyCmap`.
 #
-# _Note:_ the commented out `pip install pycmap` call does not need to be repeated every time. However, if the `pyimport` call later returns an error then please try uncommenting the `pip install pycmap` call and start over.
+# **Note:** 
+#
+# The commented out `pip install pycmap` call does not need to be repeated every time. However, if the `pyimport` call later returns an error then please try uncommenting the `pip install pycmap` call and start over.
 
 # +
 #run(`pip install pycmap`) #pycmap is used via PyCall later
@@ -36,38 +38,38 @@ import Pkg; Pkg.add("PyCall"); Pkg.build("PyCall")
 
 using PyCall
 PyCmap = pyimport("pycmap")
+cmap = PyCmap.API(token="929df5e0-d501-11e9-9d93-a1d831a9e2b2")
 
 # #### Get the cmap data catalog
 #
-# This will create a `data/` folder should contain `catalog.csv`. Another folder named `embed/` gets created below.
+# This `df.to_csv` call creates the `catalog.csv` with the content of `df`.
 
-getCatalog = pyimport("opedia.getCatalog")
+df = cmap.get_catalog()
+df.to_csv("catalog.csv")
 
-# #### Import opedia functionalities
-
-opediareg = pyimport("opedia.plotRegional")
-opediasub = pyimport("opedia.subset")
-
-# #### Download a regional Chlorophyll data set from the Darwin model
+# #### Download a regional Chlorophyll data set from the Darwin model (1/2)
 #
-# This will download a file called `data/RM_tblDarwin_Nutrient_3day_FeT_darwin_3day.csv`.
+# The `plot_map` function can in principle export data to file when `exportDataFlag=true`.
 
 # +
-tables = ["tblDarwin_Nutrient_3day"] # see catalog.csv  for the complete list of tables and variable names
-variables = ["FeT_darwin_3day"] # see catalog.csv  for the complete list of tables and variable names   
+tables = ["tblDarwin_Nutrient"] # see catalog.csv  for the complete list of tables and variable names
+variables = ["FeT"] # see catalog.csv  for the complete list of tables and variable names   
 startDate = "1994-01-03"
 endDate = "1994-01-03"
 lat1, lat2 = 10, 70
 lon1, lon2 = -180, -80
 depth1, depth2 = 0, 10
 fname = "regional"
-exportDataFlag = true # false if you you do not want to download data
+exportDataFlag = false # set to true if you want to download data
 
-opediareg.regionalMap(tables, variables, startDate, endDate, lat1, lat2, lon1, lon2, depth1, depth2, fname, exportDataFlag)
+viz = pyimport("pycmap.viz")
+viz.plot_map(tables, variables, startDate, endDate, 
+    lat1, lat2, lon1, lon2, depth1, depth2, fname, exportDataFlag)
 # -
 
-# #### Load the same data directly as a dataframe
+# #### Download a regional Chlorophyll data set from the Darwin model (2/2)
+#
+# The `space_time` function returns the same data to memory as a dataframe.
 
-df = opediasub.spaceTime(tables[1], variables[1], startDate, endDate, lat1, lat2, lon1, lon2, depth1, depth2)
-
-
+df = cmap.space_time(tables[1], variables[1], startDate, endDate, 
+    lat1, lat2, lon1, lon2, depth1, depth2)
